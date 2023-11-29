81b0f16a3c00044d997361a4f3a6617f56d154ad
ZeroQLi <itsrandomboy77@gmail.com>
2023-11-29 21:55:22 +0400

    :D

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zeroqli.github.io',
-    base: '/corepack',
+    site: 'https://corepack.me',
 });
